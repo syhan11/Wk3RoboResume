d68df624c696d863b92cd7bb74156698bcdedb8c
syhan11 <sueyoung.6311@gmail.com>
2019-07-02 16:59:34 -0400

    added comments

--- src/Wk3RoboResume.java
+++ src/Wk3RoboResume.java
@@ -1,3 +1,29 @@
+/*
+ * Write a program that will display a resume in an organized format.
+ * The resume must allow any user to enter and display:
+ * - a name,
+ * - an email address,
+ * - a phone number,
+ * - zero or more educational achievements each of which contain a school and a
+ *   year and a degree and major,
+ * - zero or more experiences each contains a company, title, date and description,
+ * - zero or more skills - each with a Competency Proficiency rating (Fundamental,
+ *   Novice, Intermediate, Advanced, Expert)
+ *
+ * Each data element must be either a String or other object, or a collection.
+ * Your project MUST contain multiple classes, implement composition and display
+ * in a format *close* to that shown below.
+ *
+ * This application should be able to capture information for multiple people.
+ *
+ * Done Already?
+ * Allow a person to change their name, e-mail address and/or phone number
+ * Create a recruiter class. The recruiter can search all the resumes and find those
+ * with a particular skill. You only have to search for one skill. So if you have five
+ * resumes and three contain Java then the search should return a list of those three
+ * people's names.
+ */
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
